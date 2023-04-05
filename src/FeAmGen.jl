f5709ecf7e7971a9106e019985212468401b275c
Quan-feng WU <32387646+Fenyutanchan@users.noreply.github.com>
2023-04-05 20:23:37 +0800

    Suggest renaming - `generate_multi_yaml` → `gen_integral_multi_yaml` - `indices_list` → `all_den_xpt_list` - `one_indices` → `den_xpt_list`

--- src/FeAmGen.jl
+++ src/FeAmGen.jl
@@ -22,7 +22,7 @@ using SymEngine
 using YAML
 
 export digest_seed_proc, generate_amp, generate_integral
-export generate_multi_yaml, generate_shiftUP_yaml
+export gen_integral_multi_yaml, generate_shiftUP_yaml
 export canonicalize_amp, gen_vac_reduction_ieta
 export gen_integralfamilies_yaml_str
 
