e2623281dd30631097c14b1e40439f8d732a53f1
Quan-feng WU <32387646+Fenyutanchan@users.noreply.github.com>
2023-05-24 17:09:49 +0800

    complete 4-loop

--- src/Canon.jl
+++ src/Canon.jl
@@ -6,14 +6,15 @@ const preferred_vac_mom_Dict() = Dict{Int,Vector{Vector{Basic}}}(
   3 => [ to_Basic( ["q1", "q2", "q3", "q1 + q3", "q2 + q3", "q1 + q2 + q3"] ),
          to_Basic( ["q1", "q2", "q3", "q1 + q2", "q1 + q3", "q2 + q3"] ) ],
   4 => [ to_Basic( ["q1", "q2", "q3", "q4",
-                    "q1 + q2", "q1 + q3",
+                    "q1 + q2", "q1 + q3", "q2 + q4",
                     "q1 + q2 + q3", "q1 + q2 + q4",
                     "q1 + q2 + q3 + q4"] ),
          to_Basic( ["q1", "q2", "q3", "q4",
                     "q1 + q2", "q1 + q3", "q2 + q4", "q3 + q4",
+                    "q2 + q3 + q4",
                     "q1 + q2 + q3 + q4"] ),
          to_Basic( ["q1", "q2", "q3", "q4",
-                    "q1 + q2", "q2 + q3", "q2 + q4",
+                    "q1 + q2", "q1 + q3", "q2 + q3", "q2 + q4",
                     "q1 + q2 + q4", "q2 + q3 + q4"] ) ]
 ) # end preferred_vac_mom_Dict
 ###################################
